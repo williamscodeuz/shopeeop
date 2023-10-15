a20dffc8cc0bb3f573e7cd80082d90a5fb252459
williamscode <uz.williamscode@gmail.com>
2023-10-16 00:03:11 +0500

    Initial commit

---
+++ src/main/java/com/williamscode/shopeeop/ShopeeopApplication.java
@@ -0,0 +1,13 @@
+package com.williamscode.shopeeop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/williamscode/shopeeop/ShopeeopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamscode.shopeeop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
